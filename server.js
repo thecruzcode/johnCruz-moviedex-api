546a1defa8b4f54e21fae8b5000f1b87c774ec36
John Cruz <jbcruz0209@gmail.com>
2019-11-16 02:27:47 -0800

    latest commit for moviedex

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 const movies = require("./movies.json");
 
 console.log(process.env.API_TOKEN);
@@ -7,9 +10,39 @@ console.log(process.env.API_TOKEN);
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
 
 app.get("/movies", function handleGetMovies(req, res) {
   let response = movies;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movies.genre.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movies => Number(movies.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
 
   res.json(response); // invoke the json response
 });
