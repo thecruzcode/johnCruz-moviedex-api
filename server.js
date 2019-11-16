1d62653982ceb69afdce1de682bd45a3eeea958b
John Cruz <jbcruz0209@gmail.com>
2019-11-16 02:27:23 -0800

    latest commit for moviedex

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const morgan = require("morgan");
 const movies = require("./movies.json");
 
+console.log(process.env.API_TOKEN);
+
 const app = express();
 
 app.use(morgan("dev"));
 
-app.get("/movies", function handleGetMovie(req, res) {
-  let;
+app.get("/movies", function handleGetMovies(req, res) {
+  let response = movies;
+
+  res.json(response); // invoke the json response
 });
 
 const PORT = 8000;
