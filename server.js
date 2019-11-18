0efe4a13c00023741fc23800a7e5360069b2a0b4
John Cruz <jbcruz0209@gmail.com>
2019-11-18 08:54:19 -0800

    made changes to server.js syntax

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const helmet = require("helmet");
 const cors = require("cors");
-const movies = require("./movies.json");
-
-console.log(process.env.API_TOKEN);
+const helmet = require("helmet");
+const MOVIES = require("./movies.json");
 
 const app = express();
 
 app.use(morgan("dev"));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
@@ -23,24 +21,24 @@ app.use(function validateBearerToken(req, res, next) {
   next();
 });
 
-app.get("/movies", function handleGetMovies(req, res) {
-  let response = movies;
+app.get("/movie", function handleGetMovies(req, res) {
+  let response = MOVIES;
 
   if (req.query.genre) {
     response = response.filter(movie =>
-      movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
     );
   }
 
   if (req.query.country) {
     response = response.filter(movie =>
-      movies.genre.toLowerCase().includes(req.query.country.toLowerCase())
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
     );
   }
 
   if (req.query.avg_vote) {
     response = response.filter(
-      movies => Number(movies.avg_vote) >= Number(req.query.avg_vote)
+      movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
     );
   }
 
