0c3e531cb6c779104e829a927d782f1661b70be2
John Cruz <jbcruz0209@gmail.com>
2019-11-21 17:58:45 -0800

    deply to heroku

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
-const MOVIES = require("./movies.json");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIES = require('./movies.json');
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(cors());
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  const authToken = req.get("Authorization");
+  const authToken = req.get('Authorization');
 
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    return res.status(401).json({ error: "Unauthorized request" });
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
   }
   next();
 });
 
-app.get("/movie", function handleGetMovies(req, res) {
+app.get('/movie', function handleGetMovies(req, res) {
   let response = MOVIES;
 
   if (req.query.genre) {
@@ -41,12 +42,17 @@ app.get("/movie", function handleGetMovies(req, res) {
       movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
     );
   }
-
-  res.json(response); // invoke the json response
 });
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
